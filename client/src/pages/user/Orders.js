8ecf2faf1c2c4b49f4975340a817de8e49f1d2fc
skkarman789 <skkarman789@gmail.com>
2023-08-27 18:38:18 +0530

    sort

--- client/src/pages/user/Orders.js
+++ client/src/pages/user/Orders.js
@@ -32,6 +32,10 @@ const Orders = () => {
   useEffect(() => {
     if (auth?.token) getOrders();
   }, [auth?.token]);
+  const reversedMemos = [...orders].reverse();
+  const getProductUrl = (productId) => {
+    return `https://metakart.com/${productId}`;
+  };
   return (
     <Layout title={"Your Orders"}>
       <div className="container-flui p-3 m-3 dashboard">
@@ -43,7 +47,18 @@ const Orders = () => {
             <h1 id="orders-h" className="text-center">
               All Orders
             </h1>
-            {orders?.map((o, i) => {
+            {reversedMemos?.map((o, i) => {
+              let formattedDate = "Invalid Date";
+              try {
+                const dateObject = new Date(o?.createdAt);
+                if (!isNaN(dateObject.getTime())) {
+                  formattedDate = dateObject.toLocaleDateString();
+                }
+              } catch (error) {
+                console.error("Error formatting date:", error);
+              }
+
+              
               return (
                 <div className="border shadow">
                   <table className="table">
@@ -52,7 +67,7 @@ const Orders = () => {
                         <th scope="col">#</th>
                         <th scope="col">Status</th>
                         <th scope="col">Buyer</th>
-                        <th scope="col">Date</th>
+                        <th scope="col">&nbsp;&nbsp;&nbsp;Date</th>
                         <th scope="col">Payment</th>
                         <th scope="col">Quantity</th>
                       </tr>
@@ -62,90 +77,98 @@ const Orders = () => {
                         <td>{i + 1}</td>
                         <td>{o?.status}</td>
                         <td>{o?.buyer?.name}</td>
-                        <td>{moment(o?.createAt).fromNow()}</td>
+                        <td>{formattedDate}</td>
                         <td>{o?.payment.success ? "Success" : "Failed"}</td>
                         <td>{o?.products?.length}</td>
                       </tr>
                     </tbody>
                   </table>
                   <div className="container" id="box-user">
-                    {o?.products?.map((p, i) => (
-                      <div className="row  mb-2 p-3 card flex-row" key={p._id}>
-                        <div className="col-md-4">
-                          <img
-                            src={`/api/v1/product/product-photo/${p._id}`}
-                            className="card-img-top"
-                            alt={p.name}
-                            width="70%"
-                            // height="auto"
-                          />
-                        </div>
-                        <div id="product-d" className="col-md-8">
-                          <p></p>
-                          <p>
-                            <span className="product-t">Name</span> : {p.name}
-                          </p>
-                          <p>
-                            <span className="product-t">Description</span> :{" "}
-                            {p.description}
-                          </p>
-                          <p>
-                            <span className="product-t">Price</span> : ₹
-                            {p.price}
-                          </p>
+                  {o?.products?.map((p, i) => {
+                      const encodedQuote = encodeURIComponent(
+                        "Check out this product: " + p.name
+                      );
 
-                          <Box
-                            style={{
-                              display: "flex",
-                              justifyContent: "flex",
-                              paddingRight: "20px",
-                              marginTop: "28%",
-                              marginRight: "80%",
-                              margin: "0%",
-                            }}
-                          >
-                            <InstapaperShareButton
-                              url="https://www.flipkart.com/"
-                              quote="ajhca"
-                              hashtag="#flipkart"
+                      return (
+                        <div
+                          className="row mb-2 p-3 card flex-row"
+                          key={p._id}
+                        >
+                          <div className="col-md-4">
+                            <img
+                              src={`/api/v1/product/product-photo/${p._id}`}
+                              className="card-img-top"
+                              alt={p.name}
+                              width="70%"
+                            />
+                          </div>
+                          <div id="product-d" className="col-md-8">
+                            <p></p>
+                            <p>
+                              <span className="product-t">Name</span> : {p.name}
+                            </p>
+                            <p>
+                              <span className="product-t">Description</span> :{" "}
+                              {p.description}
+                            </p>
+                            <p>
+                              <span className="product-t">Price</span> : ₹
+                              {p.price}
+                            </p>
+
+                            <Box
+                              style={{
+                                display: "flex",
+                                justifyContent: "flex",
+                                paddingRight: "20px",
+                                marginTop: "28%",
+                                marginRight: "80%",
+                                margin: "0%",
+                              }}
                             >
-                              <Instagram
-                                style={{
-                                  fontSize: "36px", // Adjust the icon size as needed
-                                  marginRight: "20px", // Adjust as needed for spacing between icons
-                                  cursor: "pointer", // Add a pointer cursor for better UX
-                                }}
-                              />
-                            </InstapaperShareButton>
-                            <FacebookShareButton
-                              url="https://www.flipkart.com/"
-                              quote="ajhca"
-                              hashtag="#flipkart"
-                            >
-                              <Facebook
-                                style={{
-                                  fontSize: "36px", // Adjust the icon size as needed
-                                  marginRight: "20px", // Adjust as needed for spacing between icons
-                                  cursor: "pointer", // Add a pointer cursor for better UX
-                                }}
-                              />
-                            </FacebookShareButton>
-                            <TwitterShareButton
-                              url="https://www.flipkart.com/"
-                              quote="ajhca"
-                              hashtag="#flipkart"
-                            >
-                              <Twitter
-                                style={{
-                                  fontSize: "36px", // Adjust the icon size as needed
-                                  cursor: "pointer", // Add a pointer cursor for better UX
-                                }}
-                              />
-                            </TwitterShareButton>
-                          </Box>
+                              <InstapaperShareButton
+                                url={getProductUrl(p._id)}
+                                quote={encodedQuote}
+                                hashtag="#flipkart"
+                              >
+                                <Instagram
+                                  style={{
+                                    fontSize: "36px",
+                                    marginRight: "20px",
+                                    cursor: "pointer",
+                                  }}
+                                />
+                              </InstapaperShareButton>
+                              <FacebookShareButton
+                                url={getProductUrl(p._id)}
+                                quote={encodedQuote}
+                                hashtag="#flipkart"
+                              >
+                                <Facebook
+                                  style={{
+                                    fontSize: "36px",
+                                    marginRight: "20px",
+                                    cursor: "pointer",
+                                  }}
+                                />
+                              </FacebookShareButton>
+                              <TwitterShareButton
+                                url={getProductUrl(p._id)}
+                                quote={encodedQuote}
+                                hashtag="#flipkart"
+                              >
+                                <Twitter
+                                  style={{
+                                    fontSize: "36px",
+                                    cursor: "pointer",
+                                  }}
+                                />
+                              </TwitterShareButton>
+                            </Box>
+                          </div>
                         </div>
-                      </div>
-                    ))}
+                      );
+                    })}
                   </div>
                 </div>
               );
